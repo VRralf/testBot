42623f72e77202a381de837e2e8565640c651634
Eduardo Rafael Mendoza <vrralf@gmail.com>
2023-05-16 16:13:39 -0300

    last changes

--- database.js
+++ database.js
@@ -1,5 +1,112 @@
-const MENTORES = ['#5872','#0467','#8481','#4880']
-const ALUMNOS = ['#5138']
-const CONSULTAS = ['pregunta','preguntar','consulta','consultar','duda']
+const XLSX = require('xlsx');
 
-module.exports = { MENTORES, CONSULTAS, ALUMNOS }
+const ADMINISTRADORES = [
+    'Ema#0467',
+    'Flor.#4880',
+    'camiladomato#1148',
+]
+const MENTORES = [
+    'Ema#0467',
+    'igna.borraz#7609',
+    'LucasSilva#9465',
+    'Sebastian Alfonso#6569',
+    'Eduardo Mendoza#7589',
+    'Aymará#5492',
+    'Nicolas Cirulli#3338',
+    'David Cosio#9214',
+    'camiladomato#1148',
+    'DamianOscar#2911',
+    'Guille#8481',
+    'Eric Rodriguez#2797',
+    'Einuvy#6102',
+    'Facu Araujo#5872',
+    'Fede Rouyere#9935',
+    'Guille#2253',
+    'Kevin Darnet#3494',
+    'Lopez Zaccaro Jose#1000',
+    'LucreciaGillone#5565',
+    'Sil Gutierrez#9350',
+    'DeddKal#7613',
+    'Flor.#4880'
+]
+let ALUMNOS = []
+const CONSULTAS = ['pregunta', 'preguntar', 'consulta', 'consultar', 'duda']
+
+function getAlumnos() {
+    // Ruta relativa al archivo XLSX
+    const filePath = './data/data.xlsx';
+
+    // Lee el archivo XLSX utilizando la biblioteca xlsx
+    const workbook = XLSX.readFile(filePath);
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    // Obtiene las celdas utilizadas en la hoja de cálculo
+    const range = XLSX.utils.decode_range(worksheet['!ref']);
+    const rows = [];
+    const headerRow = [];
+    for (let col = range.s.c; col <= range.e.c; col++) {
+        const cellAddress = XLSX.utils.encode_cell({ r: range.s.r, c: col });
+        const cell = worksheet[cellAddress];
+        const columnName = cell ? cell.v : null;
+        headerRow.push(columnName);
+    }
+    for (let row = range.s.r + 1; row <= range.e.r; row++) {
+        const rowData = {};
+        for (let col = range.s.c; col <= range.e.c; col++) {
+            const cellAddress = XLSX.utils.encode_cell({ r: row, c: col });
+            const cell = worksheet[cellAddress];
+            const columnName = headerRow[col];
+            rowData[columnName] = cell ? cell.v : null;
+        }
+        rows.push(rowData);
+    }
+
+    ALUMNOS = rows
+}
+
+function whoAreYou(discordTag) {
+    console.log(discordTag);
+    let roles = []
+    let admin = ADMINISTRADORES.find(admin => admin == discordTag)
+    if (admin) {
+        roles.push('Administrador')
+    }
+    let mentor = MENTORES.find(m => m == discordTag)
+    if (mentor) {
+        roles.push('Equipo Académico')
+    }
+    let alumno = ALUMNOS.find(a => a.discord == discordTag)
+    if (alumno) {
+        roles.push('Alumno cohort 48')
+    }
+    if (roles.length == 0) {
+        roles.push('Sin registrar')
+    }
+    return roles
+}
+
+function isConsulta(msj) {
+    for (word of CONSULTAS) {
+        if (msj.toLowerCase().includes(word.toLowerCase())) {
+            return true
+        }
+    }
+    return false
+}
+
+function mentorName(discordTag){
+    let mentor = MENTORES.find(m => m == discordTag)
+    return `Capo - ${mentor}`.slice(0,-5)
+}
+
+function nickName(discordTag) {
+    let alumno = ALUMNOS.find(al => al.discord == discordTag)
+    return `${alumno.Nombre} ${alumno.Apellido}`
+}
+
+function sinRegistrar(discordTag) {
+    console.log(discordTag);
+    return 'Sin Registrar /' + discordTag
+}
+
+module.exports = { MENTORES, CONSULTAS, ALUMNOS, getAlumnos, isConsulta, whoAreYou, nickName, mentorName, sinRegistrar }
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const { Client, GatewayIntentBits, Guild } = require('discord.js')
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] })
-const { MENTORES, ALUMNOS, CONSULTAS } = require('./database')
+const { MENTORES, ALUMNOS, CONSULTAS, getAlumnos, isConsulta, whoAreYou, nickName, mentorName, sinRegistrar } = require('./database')
 
 client.on('ready', () => {
   console.log(`Bot is ready as ${client.user.tag}`);
+  try {
+    getAlumnos()
+    console.log('Alumnos cargados correctamente');
+  } catch (error) {
+    console.log('Error al cargar alumnos');
+    console.log(error);
+  }
 })
 
-client.on('messageCreate',async message => {
-  if(message.system){
+client.on('messageCreate', async message => {
+  if (message.system) {
     return
   }
   console.log(`${message.author.tag} dijo: ${message.content}`);
@@ -16,19 +23,16 @@ client.on('messageCreate',async message => {
     return
   }
 
-
-
   //Borrar mensajes
   const channel = message.channel;
   const consultasChannel = message.guild.channels.cache.find(ch => ch.name === 'consultas')
-  /* console.log(channel.name);
-  return */
+
   if (message.content.includes('!borrar')) {
     // Obtener el canal del mensaje
 
     try {
       // Obtener los últimos 10 mensajes del canal
-      const messages = await channel.messages.fetch({ limit: 10 });
+      const messages = await channel.messages.fetch({ limit: 100 });
 
       // Borrar los mensajes
       channel.bulkDelete(messages);
@@ -42,7 +46,252 @@ client.on('messageCreate',async message => {
     }
     return
   }
-  if (!message.author.tag.includes('#0642')) {
+
+  /* if (message.content.includes('!verificar')) {
+    if (whoAreYou(message.author.tag).includes('Equipo Académico')) {
+      console.log('podes verificar a los alumnos');
+
+      const guild = message.guild;
+
+      // Iterar sobre todos los miembros del servidor
+      guild.members.cache.forEach(member => {
+        if (member.user.tag.includes("Mart-1#0642")) {
+          return
+        }
+        let discordRoles = whoAreYou(member.user.tag)
+        discordRoles.forEach(role => {
+          let roleA = member.guild.roles.cache.find(roleF => roleF.name == role)
+          member.roles.add(roleA)
+          switch (role) {
+            case 'Equipo Académico':
+              channel.send(`Un nuevo mentor se ha sumado al cohorte!`);
+              member.setNickname(mentorName(member.user.tag))
+              break;
+            case 'Alumno cohort 48':
+              member.setNickname(nickName(member.user.tag))
+              channel.send(`Bienvenido al curso ${member.nickname}`);
+              break;
+            case 'Sin registrar':
+              member.setNickname(sinRegistrar(member.user.tag))
+              channel.send(`Bienvenido al curso ${member.nickname}`);
+              break;
+            default:
+              break;
+          }
+        })
+        message.reply('Usuarios verificados!')
+      }
+    } else {
+      console.log('no tenes permisos para esto');
+    }
+    return
+  } */
+
+  if (message.content.includes('mensaje super secreto, dificil e imposible de saber!')) {
+    message.reply('Encontraste el codigo super secreto, dificil e imposible de saber. Lastima que no hallas ganado nada por ello... xD')
+    return
+  }
+
+  if (message.content === '!seeAll') {
+    // Código para manejar el comando '!seeAll'
+    message.guild.members.fetch().then(fetchedMembers => {
+      message.channel.send(`Total members: ${fetchedMembers.size}`);
+      let memberString = '';
+      fetchedMembers.forEach(member => {
+        let roleString = member.roles.cache.map(role => role.name).join(', ');
+        memberString += `${member.user.tag} - Roles: ${roleString}\n`;
+        if (memberString.length >= 1900) {
+          message.channel.send(memberString);
+          memberString = '';
+        }
+      });
+      if (memberString.length > 0) {
+        message.channel.send(memberString);
+      }
+    });
+    return; // Se detiene aquí si el comando es '!seeAll'
+  }
+
+  /*   if (message.content === '!seeAll') {
+      // Verificar si el mensaje proviene de un canal de texto y no es enviado por el bot mismo
+      console.log('see all');
+      console.log(message.channel.type === 'text');
+      console.log(message.channel.type);
+      console.log(!message.author.bot);
+      if (message.channel.type === 'text' && !message.author.bot) {
+        console.log('texto y no bot');
+        try {
+          // Obtener el servidor actual
+          const server = message.guild;
+  
+          // Obtener todos los miembros del servidor
+          await server.members.fetch();
+  
+          // Obtener todos los miembros del servidor (incluidos los miembros offline)
+          const members = server.members.cache;
+  
+          let memberList = '';
+  
+          // Iterar a través de la colección de miembros
+          members.forEach(member => {
+            memberList += `Nombre: ${member.user.username}, ID: ${member.user.id}\n`;
+          });
+  
+          // Enviar la lista de miembros como mensaje directo al usuario que envió el comando
+          message.author.send(`Lista de miembros:\n\n${memberList}`)
+            .then(() => {
+              console.log('Lista de miembros enviada al usuario');
+            })
+            .catch(error => {
+              console.error('Error al enviar la lista de miembros:', error);
+            });
+        } catch (error) {
+          console.error('Error al obtener los miembros del servidor:', error);
+        }
+      }
+      return
+    } */
+
+  if (message.content.toLowerCase().includes('atrás abajo adelante piña')) {
+    // Código para manejar el comando del movimiento especial de Ryu
+    message.channel.send('Ryu: "HADOUKEN!"\nhttps://media.giphy.com/media/A1rZwNM9QVfos/giphy.gif');
+    return; // Se detiene aquí si el mensaje incluye 'atrás abajo adelante piña'
+  }
+
+  if (message.content.includes('!verificar')) {
+    if (whoAreYou(message.author.tag).includes('Equipo Académico')) {
+      console.log('Podes verificar');
+      const guild = message.guild
+
+      await guild.members.fetch();
+
+      let memberList = '';
+
+      guild.members.cache.forEach(member => {
+
+        memberList += `Nombre: ${member.user.username}, ID: ${member.user.id}\n`;
+
+        if (member.user.tag.includes("Mart-1#0642")) {
+          console.log('No cambiar el nombre del bot!');
+          return
+        }
+
+        if (member.id === member.guild.ownerID) {
+          // El miembro es el propietario del servidor
+          console.log('No cambiar el nombre del propietario del servidor!');
+          return
+        }
+
+        let discordRoles = whoAreYou(member.user.tag)
+        discordRoles.forEach(role => {
+          let roleA = member.guild.roles.cache.find(roleF => roleF.name == role)
+          member.roles.add(roleA)
+          switch (role) {
+            case 'Equipo Académico':
+              member.setNickname(mentorName(member.user.tag))
+              channel.send(`Se há renombrado a ${member.nickname}`);
+              break;
+            case 'Alumno cohort 48':
+              member.setNickname(nickName(member.user.tag))
+              channel.send(`Se há renombrado a ${member.nickname}`);
+              break;
+            case 'Sin registrar':
+              member.setNickname(sinRegistrar(member.user.tag))
+              channel.send(`Se há renombrado a ${member.nickname}`);
+              member.send('Por favor revisar la situacion personal con los mentores. No tenes roles asignados en el servidor aun!')
+              break;
+            default:
+              break;
+          }
+        })
+
+        console.log('Cambiar nombre de ' + member.user.tag);
+      })
+
+      message.reply(`Lista de miembros:\n\n${memberList}`)
+        .then(() => {
+          console.log('Lista de miembros enviada al usuario');
+        })
+        .catch(error => {
+          console.error('Error al enviar la lista de miembros:', error);
+        });
+
+
+    } else {
+      console.log('No podes verificar');
+      channel.send('Tú no puedes realizar esta acción! XD')
+    }
+    return
+  }
+
+  /* if (message.content === '!verificame') {
+    try {
+      let discordRoles = whoAreYou(message.author.tag)
+      discordRoles.forEach(role => {
+        let roleA = message.member.guild.roles.cache.find(roleF => roleF.name == role)
+        message.member.roles.add(roleA)
+        switch (role) {
+          case 'Equipo Académico':
+            message.member.setNickname(mentorName(message.author.tag))
+            channel.send(`Se há renombrado a ${message.member.nickname}`);
+            break;
+          case 'Alumno cohort 48':
+            message.member.members.memberetNickname(nickName(message.author.tag))
+            channel.send(`Se há renombrado a ${message.member.nickname}`);
+            break;
+          case 'Sin registrar':
+            message.member.setNickname(sinRegistrar(message.author.tag))
+            channel.send(`Se há renombrado a ${message.member.nickname}`);
+            message.author.send('Por favor revisar la situacion personal con los mentores. No tenes roles asignados en el servidor aun!')
+            break;
+        }
+      })
+    } catch (error) {
+      message.reply('No se ha podido verificar!')
+      console.log(error);
+    }
+
+    return
+
+  } */
+
+  if (message.content === '!verificame') {
+    try {
+        // Obtiene los roles por su nombre
+        const adminRole = message.guild.roles.cache.find(role => role.name === "Administrador");
+        const academicTeamRole = message.guild.roles.cache.find(role => role.name === "Equipo Académico");
+        const studentRole = message.guild.roles.cache.find(role => role.name === "Alumno cohort 48");
+        const unregisteredRole = message.guild.roles.cache.find(role => role.name === "Sin registrar");
+
+        // Usa tu función whoAreYou para determinar los roles del miembro
+        const memberRoles = whoAreYou(message.member.user.tag);
+
+        // Elimina los roles antiguos del miembro
+        //message.member.roles.remove([adminRole, academicTeamRole, studentRole, unregisteredRole]).catch(err => console.error("Error al eliminar roles antiguos: ", err));
+
+        // Asigna los roles al miembro y cambia su apodo según su rol
+        memberRoles.forEach(roleName => {
+            if (roleName === 'Administrador') {
+                message.member.roles.add(adminRole).catch(err => console.error("Error al agregar el rol de Administrador: ", err));
+            } else if (roleName === 'Equipo Académico') {
+                message.member.roles.add(academicTeamRole).catch(err => console.error("Error al agregar el rol de Equipo Académico: ", err));
+                message.member.setNickname(`Mentor | ${message.member.user.username}`).catch(err => console.error("Error al cambiar el apodo a Mentor: ", err));
+            } else if (roleName === 'Alumno cohort 48') {
+                message.member.roles.add(studentRole).catch(err => console.error("Error al agregar el rol de Alumno: ", err));
+                message.member.setNickname(`Alumno | ${message.member.user.username}`).catch(err => console.error("Error al cambiar el apodo a Alumno: ", err));
+            } else if (roleName === 'Sin registrar') {
+                message.member.roles.add(unregisteredRole).catch(err => console.error("Error al agregar el rol de Sin registrar: ", err));
+                message.member.setNickname(`NR | ${message.member.user.username}`).catch(err => console.error("Error al cambiar el apodo a NR: ", err));
+            }
+        });
+    } catch (error) {
+        console.error(`Error general al verificar al miembro: ${error}`);
+    }
+}
+
+
+  /* if (!message.author.tag.includes('#0642')) { */
+  if (!message.author.bot) {
     /* message.reply(message.channel.name)
     message.reply('Recorda mencionar a @equipo-academico para que resuelvan tu consulta') */
     // Verificar si el mensaje contiene menciones a usuarios
@@ -52,97 +301,54 @@ client.on('messageCreate',async message => {
       message.reply('Menciones: ' + nombres)
     }
 
-    let isConsulta = false
-    CONSULTAS.forEach(word => {
-      if(message.content.includes(word)){
-        isConsulta = true
-      }
-    })
-
-    if(channel.name != 'consultas' && isConsulta){
+    if (channel.name != 'consultas' && isConsulta(message.content)) {
       message.reply(`Recorda que para hacer consultas debes hacerlo en el canal ${consultasChannel.toString()}`)
       return
     }
 
-    if (message.mentions.roles.size > 0) {
-      console.log('El mensaje menciona a un rol dentro del servidor.');
-    } else {
-      let roleMentor = message.guild.roles.cache.find(r => r.name === 'Mentores')
-      let roleAlumno = message.guild.roles.cache.find(r => r.name === 'Alumnos')
+    if (channel.name == 'consultas' && message.mentions.roles.size == 0 && isConsulta(message.content)) {
+      let roleMentor = message.guild.roles.cache.find(r => r.name === 'Equipo Académico')
+      let roleAlumno = message.guild.roles.cache.find(r => r.name === 'Alumno cohort 48')
       message.reply(`No mencionaste a nadie en este mensaje, recorda llamar a los ${roleMentor.toString()} o a los ${roleAlumno.toString()}`)
-    }
-
-    // Verificar si el mensaje contiene menciones a canales
-    if (message.mentions.channels.size > 0) {
-      console.log('El mensaje menciona a canales.');
-      message.reply('Menciones: ' + JSON.stringify(message.mentions.channels.join("/")))
+      return
     }
   }
-
-  
-
-
 });
 
 client.on('guildMemberAdd', member => {
   console.log(`${member.user.tag} se ha unido al servidor!`);
   // Enviar mensaje de bienvenida al canal general del servidor
-
   const channel = member.guild.channels.cache.find(channel => channel.name === 'general');
   if (!channel) return console.error(`El canal "general" no se encontró en ${member.guild.name}.`);
   channel.send(`Ola ke ase ${member.user.tag}`);
 
-
-  if (member.user.tag.includes('#5872')) {
-    console.log('Entro el colo!');
-    channel.send(`Le Entró al Colo`);
-    member.setNickname('El Colo')
-  }
-  if (member.user.tag.includes('#0467')) {
-    console.log('Entro el Ema');
-    channel.send(`Entro el Ema`);
-    member.setNickname('Emmanuel [Mentor]')
-  }
-  if (member.user.tag.includes('#8481')) {
-    console.log('Entro el Guille');
-    channel.send(`Entro el Guille`);
-    member.setNickname('El Guille')
-  }
-  if (member.user.tag.includes('#4880')) {
-    console.log('Ingreso la Flor');
-    channel.send(`Ingreso la Flor`);
-    member.setNickname('Floricienta [Mentor]')
-  }
-  let isMentor = false
-  let isAlumno = false
-  MENTORES.forEach(tag => {
-    if(member.user.tag.includes(tag)){
-      isMentor = true
+  let discordRoles = whoAreYou(member.user.tag)
+  discordRoles.forEach(role => {
+    let roleA = member.guild.roles.cache.find(roleF => roleF.name == role)
+    member.roles.add(roleA)
+    switch (role) {
+      case 'Equipo Académico':
+        channel.send(`Un nuevo mentor se ha sumado al cohorte!`);
+        member.setNickname(mentorName(member.user.tag))
+        /* member.setNickname(nickName('Mentor - ' + member.user.tag)) */
+        break;
+      case 'Alumno cohort 48':
+        member.setNickname(nickName(member.user.tag))
+        channel.send(`Bienvenido al curso ${member.nickname}`);
+        break;
+      case 'Sin registrar':
+        member.setNickname(sinRegistrar(member.user.tag))
+        channel.send(`Bienvenido al curso ${member.nickname}`);
+        break;
+      default:
+        break;
     }
   })
-
-  ALUMNOS.forEach(tag => {
-    if(member.user.tag.includes(tag)){
-      isAlumno = true
-    }
-  })
-  
-  let role = null
-  let roleString = 'Anonimo'
-  if(isMentor){
-    roleString = 'Mentores'
-    channel.send(`Un nuevo Crack ha aparecido para salvar las papas.`);
-  } else if(isAlumno) {
-    roleString = 'Alumnos'
-    channel.send(`Mensaje bonito de bienvenida para el alumno!`);
-  }
-  role = member.guild.roles.cache.find(role => role.name == roleString)
-  member.roles.add(role)
-  if(roleString == 'Anonimo'){
-    channel.send(`Al parecer sos un indocumentado. Si no arreglas tu situacion seras deportado!!!`);
-    member.setNickname(member.user.username + ' - Indocumentado')
-  }
 });
+
+function asignRoles() {
+
+}
 
 const { token } = require('./config.json')
 
